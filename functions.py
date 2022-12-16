4a439348231ee3d76f7c0e46f071a6e662143e46
gsr142 <76536836+gsr142@users.noreply.github.com>
2022-12-15 23:12:07 -0800

    cleaned up unused functions and added outputs to calculate button

--- functions.py
+++ functions.py
@@ -4,65 +4,25 @@ def total(a, b, c, d):
     return total
 
 # calculates barback tips
-def barback_tips_2(total_tips, bar_back_tip_percent,
-                 total_hours, emp_1_hours, emp_2_hours):
-
-    barback = round(total_tips * bar_back_tip_percent/100, 2)
-
-    barback_total_tipout = round(barback, 2)
-
-    bartender1_tipout_barback = round((barback / total_hours) * emp_1_hours, 2)
-    bartender2_tipout_barback = round((barback / total_hours) * emp_2_hours, 2)
-
-    return [barback_total_tipout, bartender1_tipout_barback, bartender2_tipout_barback]
-
-def barback_tips_3(total_tips, bar_back_tip_percent,
-                 total_hours, emp_1_hours, emp_2_hours, emp_3_hours):
-
+def barback_tips(total_tips, bar_back_tip_percent):
     barback = round(total_tips * bar_back_tip_percent / 100, 2)
+    return barback
 
-    barback_total_tipout = round(barback, 2)
-
-    bartender1_tipout_barback = round((barback / total_hours) * emp_1_hours, 2)
-    bartender2_tipout_barback = round((barback / total_hours) * emp_2_hours, 2)
-    bartender3_tipout_barback = round((barback / total_hours) * emp_3_hours, 2)
-    return [barback_total_tipout, bartender1_tipout_barback,
-            bartender2_tipout_barback, bartender3_tipout_barback]
-
-def barback_tips_4(total_tips, bar_back_tip_percent,
-                 total_hours, emp_1_hours, emp_2_hours, emp_3_hours, emp_4_hours):
-
-    barback = round(total_tips * bar_back_tip_percent / 100, 2)
-
-    barback_total_tipout = round(barback, 2)
-
-    bartender1_tipout_barback = round((barback / total_hours) * emp_1_hours, 2)
-    bartender2_tipout_barback = round((barback / total_hours) * emp_2_hours, 2)
-    bartender3_tipout_barback = round((barback / total_hours) * emp_3_hours, 2)
-    bartender4_tipout_barback = round((barback / total_hours) * emp_4_hours, 2)
-    return [barback_total_tipout, bartender1_tipout_barback,
-            bartender2_tipout_barback, bartender3_tipout_barback,
-            bartender4_tipout_barback]
 
 
 # calculates foodrunner tips
-def foodrunner(runner, food_sales, runner_percentage):
+def foodrunner(food_sales, runner_percentage):
     food_runner = 0
-    if runner.startswith("y") or runner.startswith("Y"):
-        food_runner = food_runner + (food_sales * runner_percentage / 100)
-        return food_runner
-    else:
-        return food_runner
+    food_runner = food_runner + (food_sales * runner_percentage / 100)
+    return food_runner
+
 
 # calculates hourly rate
-def hourly_rate(total_money, total_hours, emp_1_hours, emp_2_hours, emp_3_hours,
-                emp_4_hours):
-    hourly = total_money / total_hours
-    emp_1_hourly = hourly * emp_1_hours
-    emp_2_hourly = hourly * emp_2_hours
-    emp_3_hourly = hourly * emp_3_hours
-    emp_4_hourly = hourly * emp_4_hours
-    return [hourly, emp_1_hourly, emp_2_hourly, emp_3_hourly, emp_4_hourly]
+def hourly_rate(total_money, total_hours):
+
+    hourly = float(round(total_money / total_hours, 2))
+    return hourly
+
 
 # calculates if Bartenders need to tip each other so taxation is equal
 def bartender_tipout(cc_tips, emp_total_tips):
--- web.py
+++ web.py
@@ -17,52 +17,83 @@ bt4_cc_tips = st.number_input(label="Bartender 4 credit tips")
 bt4_hours_worked = st.number_input(label="Bartender 4 hours worked")
 
 # Bar back inputs
-bar_back_present = st.text_input(label="Was there a barback? Enter y or n")
-bar_back_percentage = st.number_input(label="Enter barback tip percentage")
+
+bar_back_percentage = st.number_input(
+    label="Enter barback tip percentage (leave as 0 if no barback)")
 
 # Food runner inputs
-food_runner_present = st.text_input(label="Was there a food runner? Enter y or n")
+
 food_sales = st.number_input(label="Enter food sales")
-food_runner_percentage = st.number_input(label="Enter food runner tip percentage")
+food_runner_percentage = st.number_input(
+    label="Enter food runner tip percentage (leave as 0 if no food runner)")
 
 # Totals
 total_cc_tips = fn.total(bt1_cc_tips, bt2_cc_tips, bt3_cc_tips, bt4_cc_tips)
 total_hours = fn.total(bt1_hours_worked, bt2_hours_worked, bt3_hours_worked,
                        bt4_hours_worked)
-total_tips = st.number_input(label="Total tips")
+total_tips = st.number_input(label="Total tips (charge + cash)")
 
 if st.button(label="Calculate Tipout"):
     # Barback Payout
-    bar_back_payout = []
-    bar_back_tips = bar_back_payout
-    if bar_back_present.startswith("y") or bar_back_present.startswith("Y"):
-        if bt3_hours_worked == 0 and bt4_hours_worked == 0:
-            bar_back_payout = fn.barback_tips_2(total_tips, bar_back_percentage,
-                                                     total_hours, bt1_hours_worked,
-                                                     bt2_hours_worked)
-        elif bt4_hours_worked == 0:
-            bar_back_payout = fn.barback_tips_3(total_tips, bar_back_percentage,
-                                                     total_hours, bt1_hours_worked,
-                                                     bt2_hours_worked, bt3_hours_worked)
-        else:
-            bar_back_payout = fn.barback_tips_4(total_tips, bar_back_percentage,
-                                                     total_hours, bt1_hours_worked,
-                                                     bt2_hours_worked, bt3_hours_worked,
-                                                     bt4_hours_worked)
+
+    bar_back_payout = float(round(fn.barback_tips(total_tips, bar_back_percentage), 2))
+    bar_back_hourly = float(round(bar_back_payout / total_hours, 2))
+
+    bar1_to_barback = float(round(bar_back_hourly * bt1_hours_worked, 2))
+    if bar1_to_barback > 0:
+        st.write(f"Barback receives ${bar1_to_barback} from Bartender 1")
+
+    bar2_to_barback = float(round(bar_back_hourly * bt2_hours_worked, 2))
+    if bar2_to_barback > 0:
+        st.write(f"Barback receives ${bar2_to_barback} from Bartender 2")
+
+    bar3_to_barback = float(round(bar_back_hourly * bt3_hours_worked, 2))
+    if bar3_to_barback > 0:
+        st.write(f"Barback receives ${bar3_to_barback} from Bartender 3")
+
+    bar4_to_barback = float(round(bar_back_hourly * bt4_hours_worked, 2))
+    if bar4_to_barback > 0:
+        st.write(f"Barback receives ${bar4_to_barback} from Bartender 4")
+
+    if bar_back_payout > 0:
+        st.write(f"Barback receives a total of ${bar_back_payout}")
 
     # Food Runner Pay
-    food_runner_payout = fn.foodrunner(food_runner_present, food_sales,
-                                       food_runner_percentage)
+    food_runner_payout = float(fn.foodrunner(food_sales, food_runner_percentage))
+    if food_runner_payout > 0:
+        st.write(f"Food runner receives ${food_runner_payout}")
+
+
 
     # Bartenders Hourly rate
-    total_after_tipouts = total_tips - food_runner_payout - bar_back_tips
-    hourly_rate = fn.hourly_rate(total_after_tipouts, total_hours, bt1_hours_worked,
-                                 bt2_hours_worked, bt3_hours_worked,
-                                 bt4_hours_worked)
+    total_after_tipouts = float(total_tips - food_runner_payout - bar_back_payout)
+    try:
+        cc_hourly = fn.hourly_rate(total_cc_tips, total_hours)
+    except ZeroDivisionError:
+        st.write("Please enter employee hours")
 
+    try:
+        hourly_rate = fn.hourly_rate(total_after_tipouts, total_hours)
+    except ZeroDivisionError:
+        st.write("for at least bartenders 1 and 2")
+    # Bartender take home
 
+    try:
+        bt1_take_home = float(round(hourly_rate * bt1_hours_worked, 2))
+        st.write(f"Bartender 1 takes home ${bt1_take_home}")
 
+        bt2_take_home = float(round(hourly_rate * bt2_hours_worked, 2))
+        st.write(f"Bartender 2 takes home ${bt2_take_home}")
 
+        bt3_take_home = float(round(hourly_rate * bt3_hours_worked, 2))
+        if bt3_take_home > 0:
+            st.write(f"Bartender 3 takes home ${bt3_take_home}")
+
+        bt4_take_home = float(round(hourly_rate * bt4_hours_worked, 2))
+        if bt4_take_home > 0:
+            st.write(f"Bartender 4 takes home ${bt4_take_home}")
+    except NameError:
+        st.write("or the calculator will not work for you")
 
 
 
